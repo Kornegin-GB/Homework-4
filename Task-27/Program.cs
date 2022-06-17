4ffd44694858040027f18f344be7a7f3ceeb0115
Sergey Filippenkov <fsvmxd@yandex.ru>
2022-06-17 20:29:02 +0300

    Решение задачи 27

--- Task-27/Program.cs
+++ Task-27/Program.cs
@@ -3,3 +3,23 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+using System;
+using static System.Console;
+Clear();
+Write("Введите число: ");
+int number = Convert.ToInt32(ReadLine());
+
+WriteLine($"Сумма чисел в числе = {SummNamber(number)}");
+
+int SummNamber(int A)
+{
+   int result = 0;
+   for (int i = 0; i < A; i *= 10)
+   {
+      int num = A % 10;
+      result += num;
+      A /= 10;
+   }
+   return result;
+}
