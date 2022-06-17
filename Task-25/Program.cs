5990a3741bc8a74c9c045bcf18105b60968e177a
Sergey Filippenkov <fsvmxd@yandex.ru>
2022-06-17 20:02:18 +0300

    Решение задачи 25

--- Task-25/Program.cs
+++ Task-25/Program.cs
@@ -2,3 +2,25 @@
 
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите число А: ");
+int numberA = Convert.ToInt32(ReadLine());
+Write("Введите число B: ");
+int numberB = Convert.ToInt32(ReadLine());
+
+WriteLine(Exponentiation(numberA, numberB));
+
+int Exponentiation(int A, int B)
+{
+   int result = 1;
+   for (int i = 1; i <= B; i++)
+   {
+      result *= A;
+   }
+   return result;
+}
