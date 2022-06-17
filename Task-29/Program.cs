cd435920824a3600c620e934cce24047f6dc1816
Sergey Filippenkov <fsvmxd@yandex.ru>
2022-06-17 20:39:57 +0300

    Решение задачи 29

--- Task-29/Program.cs
+++ Task-29/Program.cs
@@ -2,3 +2,32 @@
 
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
+
+using System;
+using static System.Console;
+Clear();
+
+int[] mass = new int[8];
+
+FillAray(mass);
+PrintArray(mass);
+
+void FillAray(int[] array)
+{
+   for (int i = 0; i < array.Length; i++)
+   {
+      array[i] = new Random().Next(1, 100);
+   }
+}
+
+void PrintArray(int[] array)
+{
+   int i = 0;
+   Write("[");
+   while (i < array.Length - 1)
+   {
+      Write($"{array[i]}, ");
+      i++;
+   }
+   Write($"{array.Length - 1}]\n");
+}
